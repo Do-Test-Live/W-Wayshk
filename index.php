60edfa339e13c2b56a98041f3ab32cda89efbdd4
frog (Mugdho) <frogbid.khl@gmail.com>
2023-03-09 18:08:40 +0600

    09/03/2023

--- index.php
+++ index.php
@@ -123,27 +123,6 @@
                         </div>
 
                         <div class="other-log-in">
-                            <h6>or</h6>
-                        </div>
-
-                        <div class="log-in-button">
-                            <ul>
-                                <li>
-                                    <a href="https://www.google.com/" class="btn google-button w-100">
-                                        <img src="assets/images/inner-page/google.png" class="blur-up lazyload"
-                                             alt=""> Log In with Google
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="https://www.facebook.com/" class="btn google-button w-100">
-                                        <img src="assets/images/inner-page/facebook.png" class="blur-up lazyload"
-                                             alt=""> Log In with Facebook
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div class="other-log-in">
                             <h6></h6>
                         </div>
 
@@ -166,51 +145,6 @@
 
     <!-- Tap to top start -->
     <div class="theme-option">
-        <div class="setting-box">
-            <button class="btn setting-button">
-                <i class="fa-solid fa-gear"></i>
-            </button>
-
-            <div class="theme-setting-2">
-                <div class="theme-box">
-                    <ul>
-                        <li>
-                            <div class="setting-name">
-                                <h4>Color</h4>
-                            </div>
-                            <div class="theme-setting-button color-picker">
-                                <form class="form-control">
-                                    <label for="colorPick" class="form-label mb-0">Theme Color</label>
-                                    <input type="color" class="form-control form-control-color" id="colorPick"
-                                        value="#0da487" title="Choose your color">
-                                </form>
-                            </div>
-                        </li>
-
-                        <li>
-                            <div class="setting-name">
-                                <h4>Dark</h4>
-                            </div>
-                            <div class="theme-setting-button">
-                                <button class="btn btn-2 outline" id="darkButton">Dark</button>
-                                <button class="btn btn-2 unline" id="lightButton">Light</button>
-                            </div>
-                        </li>
-
-                        <li>
-                            <div class="setting-name">
-                                <h4>RTL</h4>
-                            </div>
-                            <div class="theme-setting-button rtl">
-                                <button class="btn btn-2 rtl-unline">LTR</button>
-                                <button class="btn btn-2 rtl-outline">RTL</button>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
         <div class="back-to-top">
             <a id="back-to-top" href="#">
                 <i class="fas fa-chevron-up"></i>
